c9a96d78e07d44998c3bfc8b1cc54a4e60d2c5f9
mfardilam <MariaA>
2021-09-26 20:12:27 -0500

    Avance Sprint 3

--- Alemmakeup/src/main/java/com/edu/unbosque/Alemmakeup/AlemmakeupApplication.java
+++ Alemmakeup/src/main/java/com/edu/unbosque/Alemmakeup/AlemmakeupApplication.java
@@ -2,8 +2,12 @@ package com.edu.unbosque.Alemmakeup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = CSVController.class)
 public class AlemmakeupApplication {
 
 	public static void main(String[] args) {
---
+++ Alemmakeup/src/main/java/com/edu/unbosque/Alemmakeup/ProductosRepository.java
@@ -0,0 +1,14 @@
+package com.edu.unbosque.Alemmakeup;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import org.springframework.data.repository.CrudRepository;
+
+import model.Productos;
+
+@Repository //Realiza operaciones CRUD
+
+public interface ProductosRepository extends JpaRepository<Productos, Integer>{
+
+}
+
